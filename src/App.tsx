251ce01a6bbfc9aedc24b9ff5f862fde4f1265ef
the-axmc <the-axmc@users.noreply.github.com>
2025-08-25 12:02:15 +0200

    Fixed linting errors. Home mint button.

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import Debugger from "./pages/Debugger.tsx";
 const AppLayout: React.FC = () => (
   <main>
     <Layout.Header
-      projectId="My App"
-      projectTitle="My App"
+      projectId="Fermentation Tracker"
+      projectTitle="Fermentation Tracker"
       contentRight={
         <>
           <nav>
@@ -39,13 +39,13 @@ const AppLayout: React.FC = () => (
     <Outlet />
     <Layout.Footer>
       <span>
-        © {new Date().getFullYear()} My App. Licensed under the{" "}
+        © {new Date().getFullYear()} Fermentation Tracker. Licensed under the{" "}
         <a
           href="http://www.apache.org/licenses/LICENSE-2.0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Apache License, Version 2.0
+          Apache License, Version 2.0. Built with Aya Labs Scaffold.
         </a>
         .
       </span>
